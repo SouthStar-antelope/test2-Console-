e232e1e1dffe83625a7264313a97c796f13d36a8
“e <duyangling421@163.com>
2020-07-03 16:09:30 +0800

    master

---
+++ js/alert.js
@@ -0,0 +1,19 @@
+function taoLu() {
+    alert("你是谁？");
+    alert("你从哪里来？");
+    alert("你要到哪里去？");
+}
+
+alert("Who are you？");
+
+taoLu();
+taoLu();
+taoLu();
+
+alert("Where are you from？");
+alert("Where are you go？");
+
+taoLu();
+
+alert("要不然带上我呗？");
+alert("我会洗衣服做饭挑水砍柴！");
